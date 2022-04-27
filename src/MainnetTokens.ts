8ee6c3588645f5cacf7f433a93481e6caeb6d08a
Ajand Mardalizad <aseedgeek@gmail.com>
2022-04-27 22:27:05 +0430

    Mainnet tokens added

---
+++ src/MainnetTokens.ts
@@ -0,0 +1,56 @@
+import { TokenList } from "./types";
+
+const MainnetTokens: TokenList = {
+  RBTC: {
+    address: "0x542fDA317318eBF1d3DEAf76E0b632741A7e677d",
+    name: "Wrapped BTC",
+    symbol: "WRBTC",
+  },
+  DOC: {
+    address: "0x2d919f19D4892381d58EdEbEcA66D5642ceF1A1F",
+    name: "RIF Dollar on Chain",
+    symbol: "RDOC",
+  },
+  MOC: {
+    address: "0x9aC7Fe28967b30e3a4E6E03286D715B42B453d10",
+    name: "MOC",
+    symbol: "MOC",
+  },
+  RIF: {
+    address: "0x2acc95758f8b5f583470ba265eb685a8f45fc9d5",
+    name: "RIF",
+    symbol: "RIF",
+  },
+  USDT: {
+    address: "0xEf213441a85DF4d7acBdAe0Cf78004E1e486BB96",
+    name: "rUSDT",
+    symbol: "rUSDT",
+  },
+  XUSD: {
+    address: "0xb5999795BE0EbB5bAb23144AA5FD6A02D080299F",
+    name: "XUSD",
+    symbol: "XUSD",
+  },
+  BPro: {
+    address: "0x440cd83c160de5c96ddb20246815ea44c7abbca8",
+    name: "BitPRO",
+    symbol: "BITP",
+  },
+  ETH: {
+    address: "0x1D931Bf8656d795E50eF6D639562C5bD8Ac2B78f",
+    name: "ETHs",
+    symbol: "ETHs",
+  },
+  BNBS: {
+    address: "0x6D9659bdF5b1A1dA217f7BbAf7dBAF8190E2e71B",
+    name: "BNBs",
+    symbol: "BNBs",
+  },
+  SOV: {
+    address: "0xEFc78fc7d48b64958315949279Ba181c2114ABBd",
+    name: "Sovryn Token",
+    symbol: "SOV",
+  },
+};
+
+export default MainnetTokens;

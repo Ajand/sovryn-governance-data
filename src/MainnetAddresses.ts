c83aa4474b6555d4cf67c144f65601c84bff90bf
Ajand Mardalizad <aseedgeek@gmail.com>
2022-04-27 12:41:53 +0430

    mainnet address added

---
+++ src/MainnetAddresses.ts
@@ -0,0 +1,9 @@
+import { ContractsAddresses } from "./types";
+
+const MainnetAddresses: ContractsAddresses = {
+  priceFeeds: {
+    bProPriceFeed: "0x389e2447e1397A8e485D658a44D845a324A338Cf",
+  },
+};
+
+export default MainnetAddresses;
